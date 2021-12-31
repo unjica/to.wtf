167172e39446ff8b2c99f2e4e54678aeb921b9eb
Sanja Malovic <sanja.malovic@kaldi.si>
2021-12-31 18:25:43 +0100

    desktop

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="background">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@font-face {
+  font-family: Alpina;
+  src: url(./fonts/GT-Alpina-Standard-Thin.ttf);
+}
+@font-face {
+  font-family: Cinetype;
+  src: url(./fonts/GT-Cinetype-Mono.ttf);
+}
+.alpina {
+  font-family: Alpina;
+}
+#app {
+  font-family: Cinetype, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #0f2a63;
+  background-color: #d9dbda;
+}
+.container {
+  max-width: 800px;
+  margin: auto
+}
+.hr {
+  width: 100%;
+  background-color: #0f2a63;
+  height: 1px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.background {
+  background-image: url(./assets/background.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div style="background-color: #B3E6BE;">
+    <div style="padding: 0 10px; display: flex; justify-content: space-between; align-content: center;" class="container">
+      <div>
+        <img width="100" src="../assets/logo.png" alt="logo" class="image">
+      </div>
+      <div>
+        <div class="menu">
+          <span class="line"></span>
+          <span class="line2"></span>
+          <span class="line3"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+.image {
+  top: -15px;
+  position: absolute;
+}
+.menu {
+  margin: 10px 0;
+  width: 30px;
+  height: 30px;
+  border-radius: 20px;
+  border: 1px solid #0f2a63;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.line {
+  width: 15px;
+  height: 2px;
+  background-color: #0f2a63;
+}
+.line2 {
+  width: 15px;
+  height: 2px;
+  background-color: #0f2a63;
+}
+.line3 {
+  width: 15px;
+  height: 2px;
+  background-color: #0f2a63;
+}
+</style>
